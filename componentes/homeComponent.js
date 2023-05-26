c7f84e4d2004fea21d89770760fd7ae2c92695bc
mariasanguesa <msanguesa3@gmail.com>
2023-05-26 18:20:31 +0200

    Añadir botón #4

--- componentes/homeComponent.js
+++ componentes/homeComponent.js
@@ -1,16 +1,24 @@
 import { Text, Image, StyleSheet, Dimensions } from 'react-native';
 import { ModoContext } from '../contextos/ModoContext';
 import { useContext } from 'react';
+import { Icon, Button } from 'react-native-elements';
 
 const windowWidth = Dimensions.get('window').width;
 
 const HomeComponent = (props) => {
+
   const { modoOscuro } = useContext(ModoContext);
+
+  const handleReserva= () => {
+    //Añadir logica
+    console.log(props.nombre);
+  };
 
   return (
     <>
-      <Image style={styles.image} source={{ uri: props.src }} />
       <Text style={[styles.name, modoOscuro && styles.nameModoOscuro]}>{props.nombre}</Text>
+      <Image style={styles.image} source={{ uri: props.src }} />
+      <Button title="Reservar" onPress={handleReserva} buttonStyle={[styles.button, modoOscuro && styles.buttonModoOscuro]} icon={<Icon style={{ marginRight: 10 }} size={18} name="calendar" type="font-awesome" color="white" />} />
     </>
   );
 }
@@ -25,11 +33,21 @@ const styles = StyleSheet.create({
   name: {
     fontSize: 16,
     fontWeight: 'bold',
+    textTransform:'uppercase',
+    margin:10
   },
   nameModoOscuro: {
     fontSize: 16,
     fontWeight: 'bold',
-    color:'white'
+    color: 'white'
+  },
+  button: {
+    backgroundColor: 'lightpink',
+    borderRadius: 8,
+  },
+  buttonModoOscuro: {
+    backgroundColor: 'darkgray',
+    borderRadius: 8,
   },
 });
 
